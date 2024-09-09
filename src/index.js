3f687e575028613437f7458ba21fb79176b4dfdd
Alexis Guzman <spartanthelexvro@gmail.com>
2024-09-08 19:53:36 -0600

    Platillas de las relaciones terminadas

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT;
 
 // Import routes
 const usersRouter = require("./routes/users");
+const reservationsRouter = require("./routes/reservations.js");
+const haveRouter = require("./routes/have.js");
 
 // Cors configuration
 app.use(cors());
@@ -17,6 +19,8 @@ app.use(bodyParser.json());
 
 // Use routes
 app.use("/users", usersRouter);
+app.use("/reservations", reservationsRouter);
+app.use("/have", haveRouter);
 
 // Start the server
 app.listen(port, () => {
